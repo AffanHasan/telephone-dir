206b721c0d28260210a575ee977d9ef9be3da559
Affan Hasan <affan.hasan@hotmail.com>
2012-10-22 14:08:21 +0500

    User Entity Revisited & added registration type in it!. Created Registration type Enumeration

--- src/main/java/org/jboss/as/quickstarts/kitchensink/model/User.java
+++ src/main/java/org/jboss/as/quickstarts/kitchensink/model/User.java
@@ -1,5 +1,9 @@
 package org.jboss.as.quickstarts.kitchensink.model;
 
+import java.io.Serializable;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.TableGenerator;
@@ -9,29 +13,34 @@ import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotEmpty;
+import org.jboss.as.quickstarts.kitchensink.model.enums.RegistrationType;
 
 /**
  * Represents an application user
  * 
  * @author AffanHasan
  */
-public class User {
+public class User implements Serializable{
+	
+	private static final long serialVersionUID = 7966562061346334686L;
 	
 	@Id
-	@TableGenerator(name = "id_generator", table = "id_generator"
-		, pkColumnName = "gen_name", valueColumnName = "gen_name", pkColumnValue = "user_id_gen")
-	@GeneratedValue(generator = "id_generator")
-	private Long id;
+	@Email(message = "Incorrect email")
+	@NotNull(message = "Email address required")
+	private String email;
+
+	@NotEmpty(message = "Password Required")
+	@Size(min = 3, message = "Password required")
+	//TODO length of column in DB ? 
+	private String password;
 	
 	@NotEmpty(message = "User name required")
 	@Size(min = 3, message = "Must contain at least 3 characters")
 	@Pattern(regexp = "[A-Za-z ]*", message = "Must contain only letters and spaces")
+	//TODO length of column in DB ?
 	private String name;
 	
-	@Email(message = "Incorrect email")
-	@NotNull(message = "Email address required")
-	private String email;
-	
-	@NotEmpty(message = "Password Required")
-	private String password;
+	@NotNull
+	@Enumerated(EnumType.ORDINAL)
+	private RegistrationType registrationType;
 }
---
+++ src/main/java/org/jboss/as/quickstarts/kitchensink/model/enums/RegistrationType.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package org.jboss.as.quickstarts.kitchensink.model.enums;
+
+/**
+ * <p>represents the user registration type</p>
+ * <p>DO NOT MODIFY THE ORDER; ALWAYS ADD NEW ENTRY TO THE END</p>
+ * @author Affan
+ */
+public enum RegistrationType {
+	
+	GENERAL,
+	FACE_BOOK;
+}
